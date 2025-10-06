a848bd7496fd8be19b0387fbe46f4e7bf4d7b813
edgarunigarro <edgarunigarro@gmail.com>
2025-10-05 20:50:29 -0500

    test

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,4 @@
-// Mark active link in current language navbar
-(function () {
-  const path = location.pathname.replace(/\/index\.html$/, "/");
-  document.querySelectorAll("nav a[data-match]").forEach((a) => {
-    const m = a.dataset.match;
-    if (path === m) a.classList.add("active");
-  });
-  document.body.classList.add("has-fab");
-})();
+
 
 // --- Drawer móvil accesible ---
 (function () {
